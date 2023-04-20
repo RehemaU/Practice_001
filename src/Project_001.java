e96a8a14e48d57e600eab0a2342c3b6548e026c1
dodo7 <dodo7@192.168.219.101>
2023-04-21 03:20:46 +0900

    update_3

--- src/Project_001.java
+++ src/Project_001.java
@@ -23,6 +23,23 @@ public class Project_001 {
 		int b1 = a1+a2;
 		System.out.println(b1);
 		System.out.println("입니다.");
+		
+		System.out.println("세번째 예제: 두개의 숫자를 입력받고 두 수의 합/차/곱/나눗셈을 결과로 출력하기.");
+		System.out.println("첫번째 숫자를 입력하세요.");
+		float c1 = scan.nextFloat();
+		System.out.println("두번째 숫자를 입력하세요.");
+		float c2 = scan.nextInt();
+		float d1 = c1+c2;
+		float d2 = c1-c2;
+		float d3 = c1*c2;
+		float d4 = c1/c2;
+		System.out.println("입력받은 두 수의 합/차/곱/나눗셈의 값은,");
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(d3);
+		System.out.println(d4);
+		System.out.println("입니다.");
+		
 		scan.close();
 	}
 
